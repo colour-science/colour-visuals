b191ec3059e3ff251964b977d5a301a7ac097285
Thomas Mansencal <thomas.mansencal@gmail.com>
2025-11-21 21:39:25 +1300

    Replace *pre-commit* with *prek*.

--- tasks.py
+++ tasks.py
@@ -41,7 +41,7 @@ __all__ = [
     "clean",
     "formatting",
     "quality",
-    "precommit",
+    "prek",
     "tests",
     "examples",
     "preflight",
@@ -160,9 +160,9 @@ def quality(
 
 
 @task
-def precommit(ctx: Context) -> None:
+def prek(ctx: Context) -> None:
     """
-    Run the "pre-commit" hooks on the codebase.
+    Run the "prek" hooks on the codebase.
 
     Parameters
     ----------
@@ -170,8 +170,8 @@ def precommit(ctx: Context) -> None:
         Context.
     """
 
-    message_box('Running "pre-commit" hooks on the codebase...')
-    ctx.run("pre-commit run --all-files")
+    message_box('Running "prek" hooks on the codebase...')
+    ctx.run("prek run --all-files")
 
 
 @task
@@ -215,7 +215,7 @@ def examples(ctx: Context) -> None:
             ctx.run(f"python {os.path.join(root, filename)}")
 
 
-@task(formatting, quality, precommit, tests, examples)
+@task(formatting, quality, prek, tests, examples)
 def preflight(ctx: Context) -> None:  # noqa: ARG001
     """
     Perform the preflight tasks.
